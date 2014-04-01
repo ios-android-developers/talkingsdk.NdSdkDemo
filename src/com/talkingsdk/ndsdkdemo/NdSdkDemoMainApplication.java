2c0a6b0595f715aa506aa20125ae7fdda394f598
mooosu <mooosu@hotmail.com>
2014-04-01 21:58:46 +0800

    import talkingsdk.NdSdkDemo.git

---
+++ src/com/talkingsdk/ndsdkdemo/NdSdkDemoMainApplication.java
@@ -0,0 +1,13 @@
+package com.talkingsdk.ndsdkdemo;
+import com.talkingsdk.MainApplication;
+
+
+public class NdSdkDemoMainApplication extends MainApplication {
+
+	@Override
+	public String getSdkObjectPackagePath() {
+		// TODO Auto-generated method stub
+		return "com.talkingsdk.nd.SdkObject";
+	}
+
+}
